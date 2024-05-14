bf35d6c560fb4077c5bb29743b4293d248b0f6ab
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2024-05-14 17:50:03 -0600

    Revert "Add Windows 64-bit"
    This reverts commit 96db6249791e0358fa8fd3c46e18d7eeff887ce0.

--- forge.config.ts
+++ forge.config.ts
@@ -49,7 +49,7 @@ const config: ForgeConfig = {
 				certificateFile: 'certificate.pfx',
 				certificatePassword: process.env.WINDOWS_CODE_SIGNING_CERT_PASSWORD,
 			},
-			[ 'win32', 'win64' ]
+			[ 'win32' ]
 		),
 		...( process.env.SKIP_DMG
 			? []
